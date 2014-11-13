d1a4379e6aec99684f34adec80ec0cfc16fb860e
wpj590246 <wpj590246@163.com>
2014-11-13 11:39:51 +0800

    DB.pm

---
+++ DB.pm
@@ -0,0 +1,57 @@
+﻿package DB;
+
+use strict;
+use warnings;
+
+use DBI;
+use Data::Dumper;
+
+
+=head1 package usage
+
+my $db = new DB;
+my $dbh = $db->get_dbh;
+
+=cut
+
+sub new(){
+	my ($class, $newconfig) = @_;
+	
+	my $self = {};
+	bless $self, $class;
+	
+	$config = $newconfig if($newconfig);
+	return $self;
+}
+
+
+
+sub get_dbh(){
+	
+	my $self = shift;
+	
+	if($self->{dbh} and $self->{dbh}->ping()){
+		return $self->{dbh};
+	}
+	else{
+		$self->{dbh} = $self->connect('192.168.0.1', '3306', '*', '××', 'apollo');
+		return $self->{dbh};
+	}
+}
+
+
+sub get_front_dbh(){
+	
+	my $self = shift;
+	
+	if($self->{front_dbh} and $self->{front_dbh}->ping()){
+		return $self->{front_dbh};
+	}
+	else{
+		$self->{front_dbh} = $self->connect('192.168.0.12', '3306', '*', '××', 'apollo');
+		return $self->{front_dbh};
+	}
+}
+
+1;
+
